d8807e343794625465c2bc1e78612a6a8d163d18
521wangdong <tufuwd@163.com>
2022-04-11 14:46:56 +0800

    添加htmlwebpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import _ from 'lodash'
 import './style.css'
-import Wzry from './wzry.jpg'
+import printMe from './print.js'
 
 function component() {
     const element = document.createElement('div');
-
-    // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    const myWzry = new Image()
-    myWzry.src = Wzry
-    element.appendChild(myWzry)
+    const btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element;
 }
 
