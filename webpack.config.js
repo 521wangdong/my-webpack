1c0f0ee291b8b7c18c93695fd6f6461b248ccc8f
521wangdong <tufuwd@163.com>
2022-04-11 17:05:56 +0800

    开发环境配置

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: '管理输出'
+      title: 'Development'
     })
 
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
+    publicPath: '/',
   },
   module: {
     rules: [
